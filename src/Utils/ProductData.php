bee30b870c68d30fe8316f4d641fff1b21f4ae02
Rayssa Marconato <rayssa.g.marconato@gmail.com>
2025-09-05 21:23:50 -0300

    feat: adicionar ProductData

---
+++ src/Utils/ProductData.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    ['id' => 1, 'nome' => 'Camiseta', 'preco' => 59.90, 'estoque' => 10],
+    ['id' => 2, 'nome' => 'Calça Jeans', 'preco' => 129.90, 'estoque' => 5],
+    ['id' => 3, 'nome' => 'Tênis', 'preco' => 199.90, 'estoque' => 3],
+    ['id' => 4, 'nome' => 'Boné', 'preco' => 29.90, 'estoque' => 25],
+    ['id' => 5, 'nome' => 'Meia', 'preco' => 15.00, 'estoque' => 50],
+];
